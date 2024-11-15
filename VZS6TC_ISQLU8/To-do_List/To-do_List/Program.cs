142b106f449d13caac38fb3a649d39408d2e27a0
Marton <palotas.marton04@gmail.com>
2024-11-15 10:23:19 +0100

    Alap metódusok, melyeket meg kell még valósítani

--- VZS6TC_ISQLU8/To-do_List/To-do_List/Program.cs
+++ VZS6TC_ISQLU8/To-do_List/To-do_List/Program.cs
@@ -1,2 +1,69 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using To_do_List;
+
+class Program
+{
+    static List<TodoItem> todoList=new ();
+    const string FilePath = "";
+
+    static void Main(string[] args)
+    {
+        LoadTodoList();
+        var commands = new Dictionary<string, Action>
+        {
+            { "1", AddTodo },
+            { "2", ListTodos },
+            { "3", RemoveTodo },
+            { "4", ExitProgram }
+        };
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("To-Do Lista Kezelő");
+            Console.WriteLine("==================");
+            Console.WriteLine("Parancsok:");
+            Console.WriteLine("1 - Teendő hozzáadása");
+            Console.WriteLine("2 - Teendők listázása");
+            Console.WriteLine("3 - Teendő törlése");
+            Console.WriteLine("4 - Kilépés");
+            Console.Write("Válassz egy lehetőséget: ");
+
+            var input = Console.ReadLine();
+
+            if (commands.TryGetValue(input ?? string.Empty, out var action))
+            {
+                action.Invoke();
+            }
+            else
+            {
+                Console.WriteLine("Ismeretlen parancs. Próbáld újra!");
+                Console.WriteLine("Nyomj egy gombot a folytatáshoz...");
+                Console.ReadKey();
+            }
+        }
+    }
+
+    static void LoadTodoList()
+    {
+        throw new NotImplementedException();
+    }
+    static void RemoveTodo()
+    {
+        throw new NotImplementedException();
+    }
+
+    static void ExitProgram()
+    {
+        throw new NotImplementedException();
+    }
+
+    static void ListTodos()
+    {
+        throw new NotImplementedException();
+    }
+
+    static void AddTodo()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ VZS6TC_ISQLU8/To-do_List/To-do_List/TodoItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace To_do_List
+{
+    public record TodoItem
+    (
+        string Description,    
+        DateTime Deadline,     
+        int Priority,          
+        string Category        
+    );
+}
